7e2e157a349fdfd8bf0313bc07af8765eb37314e
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-10-22 17:44:43 +0200

    done

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -3,8 +3,22 @@ const myButton = document.getElementById("on-light");
 const myLampOff = document.getElementById("lamp-off")
 const myLampOn = document.getElementById("lamp-on")
 
+let isOn = false
 
 myButton.addEventListener("click", () => {
-    myLampOff.classList.add("d-none")
-    myLampOn.classList.add("d-block")
+    if(!isOn){
+        myLampOff.classList.remove("d-none")
+        myLampOff.classList.add("d-block")
+        myLampOn.classList.remove("d-block")
+        myLampOn.classList.add("d-none")
+        isOn = true
+    }else{
+        myLampOff.classList.remove("d-block")
+        myLampOff.classList.add("d-none")
+        myLampOn.classList.remove("d-none")
+        myLampOn.classList.add("d-block")
+        isOn = false
+
+    }
+
 })
